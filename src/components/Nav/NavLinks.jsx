d9fb8e3a6158527f82ab014ccc5518f82aafd18f
Emrikcah <curryandr3@gmail.com>
2023-08-20 05:16:49 -0400

    create nav links component

---
+++ src/components/Nav/NavLinks.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavLinks = () => {
+  return (
+    <>
+      <li>About</li>
+      <li>Careers</li>
+      <li>Events</li>
+      <li>Products</li>
+      <li>Support</li>
+    </>
+  );
+};
+
+export default NavLinks;
