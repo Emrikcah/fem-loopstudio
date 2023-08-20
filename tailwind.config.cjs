c77599577cd1058da75e605fcc80b2ff5a5e8659
Emrikcah <curryandr3@gmail.com>
2023-08-19 22:20:13 -0400

    add global styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,33 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		screens:{
-			sm: '375px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px'
-		},
-		colors:{
-			'white':'hsl(0, 0%, 100%)',
-			'black':'hsl(0, 0%, 0%)',
-			'darkGray':'#3f3cbb',
-			'veryDarkGray':'hsl(0, 0%, 41%)',
-			red:'red'
-		},
-		extend: {
-			fontFamily:{
-				sans: ['Josefin Sans', 'sans-serif'],
-				alata: ['Alata']
-			},
-			backgroundImage:{
-				'mobile':"url('src/assets/mobile/image-hero.jpg')",
-				'desktop':"url('src/assets/desktop/image-hero.jpg')",
-			},
-			gridTemplateColumns:{
-				'13': 'repeat(5, minmax(min-content, min-content))',
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      l: "425px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
+      darkGray: "#3f3cbb",
+      veryDarkGray: "hsl(0, 0%, 41%)",
+      red: "red",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+      backgroundImage: {
+        mobile: "url('src/assets/mobile/image-hero.jpg')",
+        desktop: "url('src/assets/desktop/image-hero.jpg')",
+      },
+      gridTemplateColumns: {
+        13: "repeat(5, minmax(min-content, min-content))",
+      },
+      maxWidth: {},
+    },
+  },
+  plugins: [],
+};
