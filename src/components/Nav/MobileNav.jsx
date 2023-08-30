b521b5abd1223668213d40996602493c6fd9fda6
Emrikcah <curryandr3@gmail.com>
2023-08-29 20:48:56 -0400

    remove unnecessay markup

--- src/components/Nav/MobileNav.jsx
+++ src/components/Nav/MobileNav.jsx
@@ -4,9 +4,9 @@ import NavLinks from '@components/Nav/NavLinks'
 const MobileNav = ({MdClose,handleClose}) => {
   return (
     <nav className="font-bold text-white lg:hidden ">
-    <ul className="">
+    
       <NavLinks/>
-    </ul>
+   
     <MdClose onClick={handleClose} size={25} color="white" className="lg:hidden" />
   </nav>
   )
