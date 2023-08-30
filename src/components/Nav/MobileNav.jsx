dccfb7706eca340ebe4d59db9c5182a93197d979
Emrikcah <curryandr3@gmail.com>
2023-08-30 02:14:28 -0400

    change styles, add markup and pass props

--- src/components/Nav/MobileNav.jsx
+++ src/components/Nav/MobileNav.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import NavLinks from '@components/Nav/NavLinks'
+import React from "react";
+import NavLinks from "@components/Nav/NavLinks";
 
-const MobileNav = ({MdClose,handleClose}) => {
+const MobileNav = ({ MdClose, handleClose }) => {
   return (
-    <nav className="font-bold text-white lg:hidden ">
-    
-      <NavLinks/>
-   
-    <MdClose onClick={handleClose} size={25} color="white" className="lg:hidden" />
-  </nav>
-  )
-}
+    <nav className="font-bold text-white lg:hidden absolute top-0 bottom-0 left-0 self-end w-full min-h-screen text-lg uppercase bg-black  ">
+      <div className="section-container py-16">
+        <div className="flex justify-between">
+          <NavLinks />
 
-export default MobileNav
+          <MdClose
+            onClick={handleClose}
+            size={30}
+            color="white"
+            className="lg:hidden"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default MobileNav;
