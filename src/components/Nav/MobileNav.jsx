6467b64bbd22aa93cef4523570fd95355cc20b72
Emrikcah <curryandr3@gmail.com>
2023-09-02 13:20:37 -0400

    style changes

--- src/components/Nav/MobileNav.jsx
+++ src/components/Nav/MobileNav.jsx
@@ -1,23 +1,21 @@
 import React from "react";
+import logo from "@assets/logo.svg";
 import NavLinks from "@components/Nav/NavLinks";
 
 const MobileNav = ({ MdClose, handleClose }) => {
-  return (
-    <nav className="font-bold text-white lg:hidden absolute top-0 bottom-0 left-0 self-end w-full min-h-screen text-lg uppercase bg-black  ">
-      <div className="section-container py-16">
-        <div className="flex justify-between">
-          <NavLinks />
+   return (
+      <nav className="font-bold text-white lg:hidden absolute top-0 bottom-0 left-0  w-full min-h-screen text-lg uppercase bg-black z-10 ">
+         <div className="section-container py-10">
+            <div className="grid grid-cols-[repeat(2,auto)] grid-rows-[repeat(2,auto)] justify-between items-end">
+               <NavLinks styles="row-start-2 text-2xl font-light space-y-8  mt-40" />
 
-          <MdClose
-            onClick={handleClose}
-            size={30}
-            color="white"
-            className="lg:hidden"
-          />
-        </div>
-      </div>
-    </nav>
-  );
+               <img src={logo} alt="logo" className="w-36 " />
+
+               <MdClose onClick={handleClose} size={30} color="white" className="lg:hidden  self-start" />
+            </div>
+         </div>
+      </nav>
+   );
 };
 
 export default MobileNav;
