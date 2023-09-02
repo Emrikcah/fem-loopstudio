8a7758d8a50052e3086c42c34b643862cf16c62c
Emrikcah <curryandr3@gmail.com>
2023-09-02 13:21:03 -0400

    logic for disable scroll when mobile nav is open

--- src/components/Nav/Header.jsx
+++ src/components/Nav/Header.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "@assets/logo.svg";
 import { MdMenu, MdClose } from "react-icons/md";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleOpen = () => {
     setIsOpen(true);
@@ -14,12 +14,27 @@ const Header = () => {
     setIsOpen(false);
   };
 
+  //disable scroll when isOpen is true
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('scroll-disabled')
+    }else{
+     document.body.classList.remove('scroll-disabled')
+
+    }
+  
+    return () => {
+     document.body.classList.remove('scroll-disabled')
+    }
+  }, [isOpen])
+  
+
   return (
     <header id="header" className="header ">
-      <div className="">
+     
         <div className="flex justify-between items-center ">
-          <div className="navbar-brand">
-            <img src={logo} alt="logo" className="" />
+          <div className="w-36 lg:w-48">
+            <img src={logo} alt="logo"  />
           </div>
           {/* nav */}
           <Nav MdMenu={MdMenu} isOpen={isOpen} handleOpen={handleOpen} />
@@ -29,7 +44,7 @@ const Header = () => {
           {isOpen && <MobileNav MdClose={MdClose} handleClose={handleClose} />}
           {/* mobile nav end */}
         </div>
-      </div>
+      
     </header>
   );
 };
